5ccf5bdf7175447adc28eaca5710379cdacb6f42
Joe Boyd <josefuboyd@gmail.com>
2022-10-08 19:02:31 -0400

    Call reset-limit in -main

--- src/feedn/main.clj
+++ src/feedn/main.clj
@@ -1,6 +1,7 @@
 (ns feedn.main
   (:require [feedn.config :refer [load-config]]
             [feedn.frontend :refer [run-server!]]
+            [feedn.limit :refer [reset-limit]]
             [feedn.state :refer [state*]]
             [feedn.updater :refer [run-updater!]]))
 
@@ -9,6 +10,7 @@
 (defn -main [& args]
   (let [config (load-config "config.edn")]
     (swap! state* merge config)
+    (swap! state* reset-limit)
     (future (run-updater!))
     (run-server!)))
 
