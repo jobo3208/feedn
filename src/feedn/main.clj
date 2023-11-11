d06198a86f0132ccb36ac77c9dcf6b7f966e43d1
Joe Boyd <josefuboyd@gmail.com>
2023-11-11 17:47:30 -0500

    Add embedded nrepl server, cli opts

--- src/feedn/main.clj
+++ src/feedn/main.clj
@@ -2,17 +2,28 @@
   (:require [feedn.config :refer [load-config!]]
             [feedn.frontend :refer [run-server!]]
             [feedn.updater :refer [run-updater!]]
+            [clojure.tools.cli :refer [parse-opts]]
+            [cider.nrepl :refer [cider-nrepl-handler]]
+            [nrepl.server]
             [taoensso.timbre :as log]))
 
 (System/setProperty "sun.net.client.defaultConnectTimeout" "5000")
 (System/setProperty "sun.net.client.defaultReadTimeout" "5000")
 
-(defn -main
-  ([]
-   (-main "config.edn"))
-  ([config-filepath]
-   (log/merge-config! {:appenders {:spit (log/spit-appender {:fname "log"})}
-                       :min-level :info})
-   (load-config! config-filepath)
-   (future (run-updater!))
-   (run-server!)))
+(def cli-options
+  [["-c" "--config CONFIG" "Config filepath"
+    :default "config.edn"]
+   ["-p" "--nrepl-port PORT" "port on which to run optional nrepl server"
+    :parse-fn #(Integer/parseInt %)]])
+
+(defn -main [& args]
+  (let [opts (:options (parse-opts args cli-options))]
+    (do
+      (log/merge-config! {:appenders {:spit (log/spit-appender {:fname "log"})}
+                          :min-level :info})
+      (load-config! (:config opts))
+      (when (:nrepl-port opts)
+        (log/info (str "Starting nrepl server on port " (:nrepl-port opts)))
+        (nrepl.server/start-server :port (:nrepl-port opts) :handler cider-nrepl-handler))
+      (future (run-updater!))
+      (run-server!))))
